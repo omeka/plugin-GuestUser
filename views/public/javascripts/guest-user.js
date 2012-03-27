fd45cc75bb8b0d7c6312b5f183f055df164bbe66
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-03-27 12:03:26 -0400

    fixes wrong id

--- views/public/javascripts/guest-user.js
+++ views/public/javascripts/guest-user.js
@@ -1,6 +1,6 @@
 
 jQuery('document').ready(function() {
-    jQuery('div#guest-user-user').hover(
+    jQuery('div#guest-user-bar').hover(
             function() {jQuery('div#guest-user-dropdown-bar').show();},
             function() {jQuery('div#guest-user-dropdown-bar').hide();}
         );
