6036f2e75d5a5500b09f4fec81569a7f23171d11
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-04-07 15:01:10 -0400

    Updates widgets for post-1.5.

--- helpers/functions.php
+++ helpers/functions.php
@@ -2,7 +2,6 @@
 
 function guest_user_widget($widget)
 {
-
     if(is_array($widget)) {
         $html = "<h2 class='guest-user-widget-label'>" . $widget['label'] . "</h2>";
         $html .= $widget['content'];
