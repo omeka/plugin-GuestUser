227842ec65d141168cf8d0c20be47a547dc4cb70
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-04-07 14:04:04 -0400

    I hate typos.

--- views/public/user/register.php
+++ views/public/user/register.php
@@ -6,7 +6,7 @@ head(array('bodyclass' => 'register', 'title' => $pageTitle));
 <h1><?php echo $pageTitle; ?></h1>
 <div id='primary'>
 <div id='capabilities'>
-<?php echo get_option('guest_users_capabilities'); ?>
+<?php echo get_option('guest_user_capabilities'); ?>
 </div>
 <?php echo flash(); ?>
 <?php echo $this->form; ?>
