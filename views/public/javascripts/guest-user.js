a8a33db6668c4e8f3866f833840b4ef5882cd5ac
patrickmj <patrickmjchnm@gmail.com>
2013-01-29 12:44:13 -0500

    functional but weak fixes to positioning menus on admin bar

--- views/public/javascripts/guest-user.js
+++ views/public/javascripts/guest-user.js
@@ -1,5 +1,11 @@
 
 jQuery('document').ready(function() {
+    var adminBarHeight = jQuery('#admin-bar');
+    jQuery('#guest-user-register-info').css('top', adminBarHeight + 'px');
+    
+    var welcomeTextLeft = jQuery('#menu-admin-bar-welcome').offset().left;
+    var guMenu = jQuery('#menu-guest-user-me').closest('ul');
+    guMenu.offset({left:welcomeTextLeft, top:31});
     jQuery('a#menu-admin-bar-welcome').closest('li').hover(
             function() {jQuery('#menu-guest-user-me').closest('ul').show();},
             function() {jQuery('#menu-guest-user-me').closest('ul').hide();}
