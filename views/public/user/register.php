5604f5dc62788f0b17d31ed620dd4bf3f573c811
patrickmj <patrickmjchnm@gmail.com>
2013-05-08 11:29:23 -0400

    puts some html around what users can do on registration page

--- views/public/user/register.php
+++ views/public/user/register.php
@@ -9,7 +9,9 @@ echo head(array('bodyclass' => 'register', 'title' => $pageTitle));
 <h1><?php echo $pageTitle; ?></h1>
 <div id='primary'>
 <div id='capabilities'>
+<p>
 <?php echo get_option('guest_user_capabilities'); ?>
+</p>
 </div>
 <?php echo flash(); ?>
 <?php echo $this->form; ?>
