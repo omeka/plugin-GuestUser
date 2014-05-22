9b7063b4c669b89ef9e51c8817cc722453abf786
Kim Nguyen <design@kimisgold.com>
2014-05-22 16:05:06 -0400

    Position admin bar sub-menu with CSS instead of jQuery.

--- views/public/javascripts/guest-user.js
+++ views/public/javascripts/guest-user.js
@@ -1,22 +1,4 @@
-
 jQuery('document').ready(function() {
     var adminBarHeight = jQuery('#admin-bar');
     jQuery('#guest-user-register-info').css('top', adminBarHeight + 'px');
-    
-    var welcome = jQuery('#menu-admin-bar-welcome');
-    if(welcome.length != 0) {
-        var welcomeTextLeft = jQuery('#menu-admin-bar-welcome').offset().left;
-        var guMenu = jQuery('#menu-guest-user-me').closest('ul');
-        guMenu.offset({left:welcomeTextLeft, top:31});        
-    }
-
-    jQuery('a#menu-admin-bar-welcome').closest('li').hover(
-            function() {jQuery('#menu-guest-user-me').closest('ul').show();},
-            function() {jQuery('#menu-guest-user-me').closest('ul').hide();}
-        );
-
-    jQuery('#menu-guest-user-register').closest('li').hover(
-            function() {jQuery('#guest-user-register-info').show();},
-            function() {jQuery('#guest-user-register-info').hide();}
-        );
 });
