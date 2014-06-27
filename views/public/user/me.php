a77d10d3b339248b4b8071ea8f03d90123e51f87
Kim Nguyen <design@kimisgold.com>
2014-06-27 17:30:51 -0400

    Remove unnecessary primary container and polish widget margins.

--- views/public/user/me.php
+++ views/public/user/me.php
@@ -5,15 +5,11 @@ echo head(array('title' => $pageTitle));
 ?>
 <h1><?php echo $pageTitle; ?></h1>
 
-<div id='primary'>
 <?php echo flash(); ?>
-<p><?php echo __("Browse and manage your work here."); ?></p>
 
 <?php foreach($widgets as $index=>$widget): ?>
 <div class='guest-user-widget <?php if($index & 1): ?>guest-user-widget-odd <?php else:?>guest-user-widget-even<?php endif;?>'>
 <?php echo GuestUserPlugin::guestUserWidget($widget); ?>
 </div>
 <?php endforeach; ?>
-
-</div>
 <?php echo foot(); ?>
