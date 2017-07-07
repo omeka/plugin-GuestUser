48b734b6b4382a27e0df06321ab1509afd4d74c8
Luk Puk <lp1051@gmail.com>
2017-07-07 09:19:24 +0200

    Remove short registration info

--- config_form.php
+++ config_form.php
@@ -2,7 +2,7 @@
 
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __('Registration Features'); ?></label>    
+        <label><?php echo __('Registration Features'); ?></label>
     </div>
     <div class="inputs five columns omega" >
         <p class='explanation'><?php echo __("Add some text to the registration screen so people will know what they get for registering.
@@ -12,55 +12,41 @@
         <div class="input-block">
             <textarea name="guest_user_capabilities" ><?php echo get_option('guest_user_capabilities'); ?></textarea>
         </div>
-
     </div>
 </div>
 
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Short Registration Features"); ?></label>    
+        <label><?php echo __("Dashboard Label"); ?></label>
     </div>
-    <div class="inputs five columns omega" >
-        <p class='explanation'><?php echo __("Add a shorter version to use as a dropdown from the user bar. If empty, no dropdown will appear."); ?>
-        </p>        
-        <div class="input-block">
-            <textarea name="guest_user_short_capabilities" ><?php echo get_option('guest_user_short_capabilities'); ?></textarea>
-        </div>
-    </div>
-</div>
-
-<div class="field">
-    <div class="two columns alpha">
-        <label><?php echo __("Dashboard Label"); ?></label>    
-    </div>    
     <div class="inputs five columns omega">
         <p class='explanation'><?php echo __("The text to use for the label on the user's dashboard"); ?></p>
-        <div class="input-block">        
-            <input name="guest_user_dashboard_label" type="text" value="<?php echo get_option('guest_user_dashboard_label'); ?>" />        
+        <div class="input-block">
+            <input name="guest_user_dashboard_label" type="text" value="<?php echo get_option('guest_user_dashboard_label'); ?>" />
         </div>
     </div>
 </div>
 
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Login Text"); ?></label>    
-    </div>    
+        <label><?php echo __("Login Text"); ?></label>
+    </div>
     <div class="inputs five columns omega">
         <p class='explanation'><?php echo __("The text to use for the 'Login' link in the user bar"); ?></p>
-        <div class="input-block">        
-        <input name="guest_user_login_text" type="text" value="<?php echo get_option('guest_user_login_text'); ?>" />        
+        <div class="input-block">
+        <input name="guest_user_login_text" type="text" value="<?php echo get_option('guest_user_login_text'); ?>" />
         </div>
     </div>
 </div>
-        
+
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Register Text"); ?></label>    
-    </div>    
+        <label><?php echo __("Register Text"); ?></label>
+    </div>
     <div class="inputs five columns omega">
         <p class='explanation'><?php echo __("The text to use for the 'Register' link in the user bar."); ?></p>
-        <div class="input-block">        
-            <input name="guest_user_register_text" type="text" value="<?php echo get_option('guest_user_register_text'); ?>" />        
+        <div class="input-block">
+            <input name="guest_user_register_text" type="text" value="<?php echo get_option('guest_user_register_text'); ?>" />
         </div>
     </div>
 </div>
@@ -68,11 +54,11 @@
 
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Allow open registration?"); ?></label>    
+        <label><?php echo __("Allow open registration?"); ?></label>
     </div>
     <div class="inputs five columns omega">
         <p class="explanation"><?php echo __("Allow guest user registration without administrator approval?"); ?></p>
-        <div class="input-block">        
+        <div class="input-block">
             <?php
                 $checked = ( get_option('guest_user_open') == 1) ;
                 if($checked) {
@@ -80,27 +66,27 @@
                 } else {
                     $options = array();
                 }
-                echo $view->formCheckbox('guest_user_open', null, $options);                
+                echo $view->formCheckbox('guest_user_open', null, $options);
             ?>
         </div>
     </div>
 </div>
-        
-        
+
+
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Allow instant access?"); ?></label>    
+        <label><?php echo __("Allow instant access?"); ?></label>
     </div>
     <div class="inputs five columns omega">
         <p class="explanation"><?php echo __("Allow instant access for 20 minutes for new users"); ?></p>
-        <div class="input-block">        
+        <div class="input-block">
             <?php
                 $checked = ( get_option('guest_user_instant_access') == 1 );
                 if($checked) {
                     $options = array('checked'=>'checked');
                 } else {
                     $options = array();
-                }            
+                }
                 echo $view->formCheckbox('guest_user_instant_access', null, $options);
             ?>
         </div>
@@ -110,11 +96,11 @@
 <?php if(get_option('recaptcha_public_key') && get_option('recaptcha_private_key')): ?>
 <div class="field">
     <div class="two columns alpha">
-        <label><?php echo __("Require ReCaptcha?"); ?></label>    
+        <label><?php echo __("Require ReCaptcha?"); ?></label>
     </div>
     <div class="inputs five columns omega">
         <p class="explanation"><?php echo __("Check this to require passing a ReCaptcha test when registering"); ?></p>
-        <div class="input-block">        
+        <div class="input-block">
             <?php
                 $checked = ( get_option('guest_user_recaptcha') == 1);
                 if($checked) {
@@ -123,7 +109,7 @@
                     $options = array();
                 }
                 echo $view->formCheckbox('guest_user_recaptcha', null, $options);
-            ?>           
+            ?>
         </div>
     </div>
 </div>
--- views/public/javascripts/guest-user.js
+++
@@ -1,4 +0,0 @@
-jQuery('document').ready(function() {
-    var adminBarHeight = jQuery('#admin-bar');
-    jQuery('#guest-user-register-info').css('top', adminBarHeight + 'px');
-});
