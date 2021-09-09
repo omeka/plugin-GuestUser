bcd1f1929a502ec41cc95045f4e790d4dcf2677d
Kim Nguyen <design@kimisgold.com>
2021-09-09 15:37:14 -0400

    Add responsive table wrapper, wrap search options in labels.

--- views/admin/user/browse.php
+++ views/admin/user/browse.php
@@ -44,9 +44,9 @@ echo flash();
     <form id='search-users' method='GET'>
     <input type='text' name='search'/>
     <fieldset class="options">
-        <input type='radio' name='search-type' value='username' checked='checked' /><span><?php echo __('Usernames'); ?></span>
-        <input type='radio' name='search-type' value='name' /><span><?php echo __('Real names'); ?></span>
-        <input type='radio' name='search-type' value='email' /><span><?php echo __('Email addresses'); ?></span>
+        <label><input type='radio' name='search-type' value='username' checked='checked' /><?php echo __('Usernames'); ?></label>
+        <label><input type='radio' name='search-type' value='name' /><?php echo __('Real names'); ?></label>
+        <label><input type='radio' name='search-type' value='email' /><?php echo __('Email addresses'); ?></label>
     </fieldset>
     <button type='submit'><?php echo __('Search users'); ?></button>
     </form>
@@ -63,45 +63,47 @@ echo flash();
 </div>
 
 <?php echo pagination_links(); ?>
-<table id="users">
-    <thead>
-        <tr>
-        <?php $sortLinks = array(
-                __('ID') => 'id',
-                __('Username') => 'username',
-                __('Real Name') => 'name',
-                __('Email') => 'email',
-                __('Role') => 'role',
-                );
-        ?>
-        <?php echo browse_sort_links($sortLinks,  array('link_tag' => 'th scope="col"', 'list_tag' => '')); ?>
-        </tr>
-    </thead>
-    <tbody>
-    <?php foreach( $users as $key => $user ): ?>
-        <tr class="<?php if (current_user()->id == $user->id) echo 'current-user '; ?><?php if($key%2==1) echo 'even'; else echo 'odd'; ?><?php if(!$user->active): ?> inactive<?php endif; ?>">
-            <td>
-            <?php echo metadata($user, 'id'); ?>
+<div class="table-responsive">
+    <table id="users">
+        <thead>
+            <tr>
+            <?php $sortLinks = array(
+                    __('ID') => 'id',
+                    __('Username') => 'username',
+                    __('Real Name') => 'name',
+                    __('Email') => 'email',
+                    __('Role') => 'role',
+                    );
+            ?>
+            <?php echo browse_sort_links($sortLinks,  array('link_tag' => 'th scope="col"', 'list_tag' => '')); ?>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach( $users as $key => $user ): ?>
+            <tr class="<?php if (current_user()->id == $user->id) echo 'current-user '; ?><?php if($key%2==1) echo 'even'; else echo 'odd'; ?><?php if(!$user->active): ?> inactive<?php endif; ?>">
+                <td>
+                <?php echo metadata($user, 'id'); ?>
+                </td>
+                <td>
+                <?php echo html_escape($user->username); ?> <?php if(!$user->active): ?>(<?php echo __('inactive'); ?>)<?php endif; ?>
+                <ul class="action-links group">
+                    <?php if (is_allowed($user, 'edit')): ?>
+                    <li><?php echo link_to($user, 'edit', __('Edit'), array('class'=>'edit')); ?></li>
+                    <?php endif; ?>
+                    <?php if (is_allowed($user, 'delete')): ?>
+                    <li><?php echo link_to($user, 'delete-confirm', __('Delete'), array('class'=>'delete')); ?></li>
+                    <?php endif; ?>
+                </ul>
+                <?php fire_plugin_hook('admin_users_browse_each', array('user' => $user, 'view' => $this)); ?>
             </td>
-            <td>
-            <?php echo html_escape($user->username); ?> <?php if(!$user->active): ?>(<?php echo __('inactive'); ?>)<?php endif; ?>
-            <ul class="action-links group">
-                <?php if (is_allowed($user, 'edit')): ?>
-                <li><?php echo link_to($user, 'edit', __('Edit'), array('class'=>'edit')); ?></li>
-                <?php endif; ?>
-                <?php if (is_allowed($user, 'delete')): ?>
-                <li><?php echo link_to($user, 'delete-confirm', __('Delete'), array('class'=>'delete')); ?></li>
-                <?php endif; ?>
-            </ul>
-            <?php fire_plugin_hook('admin_users_browse_each', array('user' => $user, 'view' => $this)); ?>
-           </td>
-            <td><?php echo html_escape($user->name); ?></td>
-            <td><?php echo html_escape($user->email); ?></td>
-            <td><span class="<?php echo html_escape($user->role); ?>"><?php echo html_escape(__(Inflector::humanize($user->role))); ?></span></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+                <td><?php echo html_escape($user->name); ?></td>
+                <td><?php echo html_escape($user->email); ?></td>
+                <td><span class="<?php echo html_escape($user->role); ?>"><?php echo html_escape(__(Inflector::humanize($user->role))); ?></span></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 <?php echo pagination_links(); ?>
 <?php fire_plugin_hook('admin_users_browse', array('users' => $users, 'view' => $this)); ?>
 <script type="text/javascript">
